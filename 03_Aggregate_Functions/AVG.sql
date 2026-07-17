412754562ab2616d13efa4637663d0894bda7b30
Shristy Katiyar <80064924+shristy-17@users.noreply.github.com>
2026-07-17 14:15:30 +0530

    Create AVG.sql

---
+++ 03_Aggregate_Functions/AVG.sql
@@ -0,0 +1,13 @@
+/*
+============================================================
+Question 1: Find Average Salary
+Difficulty: Easy
+Category: AVG
+
+Problem Statement:
+Write an SQL query to calculate the average salary of employees.
+============================================================
+*/
+
+SELECT AVG(salary) AS average_salary
+FROM employees;

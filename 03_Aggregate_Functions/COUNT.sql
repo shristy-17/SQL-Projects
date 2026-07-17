228d7dd265630f0ad0ddbf8bd2f55beaf842c926
Shristy Katiyar <80064924+shristy-17@users.noreply.github.com>
2026-07-17 14:14:51 +0530

    Create COUNT.sql

---
+++ 03_Aggregate_Functions/COUNT.sql
@@ -0,0 +1,13 @@
+/*
+============================================================
+Question 1: Count Total Employees
+Difficulty: Easy
+Category: COUNT
+
+Problem Statement:
+Write an SQL query to count the total number of employees.
+============================================================
+*/
+
+SELECT COUNT(*) AS total_employees
+FROM employees;

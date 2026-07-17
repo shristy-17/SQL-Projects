73829e1f0ac27329df1539f2386353a817b86d9e
Shristy Katiyar <80064924+shristy-17@users.noreply.github.com>
2026-07-17 14:03:52 +0530

    Create WHERE.sql

---
+++ 01_Basic_SQL/WHERE.sql
@@ -0,0 +1,14 @@
+/*
+============================================================
+Question 1: Find Employees with Salary Greater Than ₹50,000
+Difficulty: Easy
+Category: WHERE
+
+Problem Statement:
+Write an SQL query to display all employees whose salary is greater than 50000.
+============================================================
+*/
+
+SELECT *
+FROM employees
+WHERE salary > 50000;
